ff512fb77b1be3f201c1bd5ae0198e751b19ab47
hane0707 <41313415+hane0707@users.noreply.github.com>
2024-05-11 07:22:51 +0900

    フッターのレイアウト変更

--- packages/haku-work/components/Footer.vue
+++ packages/haku-work/components/Footer.vue
@@ -1,12 +1,15 @@
 <template>
   <v-footer class="bg-grey-lighten-3">
     <v-row no-gutters class="text-grey-darken-1">
-      <v-col cols="12" class="d-flex flex-row justify-center m-4">
-        <v-btn nuxt to='/' variant="text"><span class="fl-nomal">TOP</span></v-btn>
-        <v-btn nuxt to='/about' variant="text"><span class="fl-nomal">ABOUT</span></v-btn>
-        <v-btn nuxt to='/works' variant="text"><span class="fl-nomal">WORKS</span></v-btn>
+      <v-col sm="12" lg="5" class="m-4 d-flex flex-row justify-center">
+        <p class="text-3xl mr-4">Haku-works</p>
       </v-col>
-      <v-col sm="12" lg="12" class="d-flex flex-row justify-center">
+      <v-col cols="5" class="m-4 hidden-sm">
+        <v-btn nuxt to='/' variant="text"><span class="fl-nomal text-xs mx-5">TOP</span></v-btn>
+        <v-btn nuxt to='/about' variant="text"><span class="fl-nomal text-xs mx-5">ABOUT</span></v-btn>
+        <v-btn nuxt to='/works' variant="text"><span class="fl-nomal text-xs mx-5">WORKS</span></v-btn>
+      </v-col>
+      <v-col sm="12" lg="5" class="d-flex flex-row justify-center">
         <a href="https://twitter.com/haku_craft1999" target="_blank" class="w-10"><img src="/img/x-logo.png" class="w-10 h-auto bg-slate-200 rounded-2xl p-2"></img></a>
       </v-col>
       <v-col class="text-center mt-4" cols="12">
